973b92f1510d5f0c2a79ba1206a784d56c219690
sitra <sitramohammed75@gmail.com>
2024-05-03 14:50:57 +0300

    new update

--- todolist/src/pages/Home.jsx
+++ todolist/src/pages/Home.jsx
@@ -12,8 +12,13 @@ function Home() {
       <h1>To-Do List</h1>
       <ul>
         {todos.map(todo => (
-          <li key={todo.id}>
-            <Link to={`/todos/${todo.id}`}>{todo.task}</Link>
+          <li key={todo.id}  className='border p-2'>
+            <Link to={`/todos/${todo.id}`} >
+               <h2>Task: {todo.task}</h2>
+      <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
+      <p>Priority: {todo.priority}</p>
+      <p>Due Date: {todo.dueDate}</p>
+      </Link>
           </li>
         ))}
       </ul>
