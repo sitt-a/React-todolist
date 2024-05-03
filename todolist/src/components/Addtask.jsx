0a766a6631d20cc4f3e5ff23e1e42424e58cadb4
sitra <sitramohammed75@gmail.com>
2024-05-03 22:12:57 +0300

    new update

--- todolist/src/components/Addtask.jsx
+++ todolist/src/components/Addtask.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Draggable from 'react-draggable';
 
-
-const Addtask = ({ onTaskAdded,onclose }) => {
+const Addtask = ({ onTaskAdded, onclose }) => {
   const [newTask, setNewTask] = useState('');
   const [priority, setPriority] = useState('');
   const [dueDate, setDueDate] = useState('');
@@ -45,57 +45,61 @@ const Addtask = ({ onTaskAdded,onclose }) => {
       },
       body: JSON.stringify(taskObject),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log('New task added:', data);
         onTaskAdded(); // Call the onTaskAdded callback after the task is added
-        onclose()
+        onclose();
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   };
+
   const handleClose = () => {
     onclose(); // Call the function passed as the onclose prop
   };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-4 rounded-lg shadow-lg">
-        <h2 className="text-xl font-bold mb-4">Add Task</h2>
-        <div>
-          <label className="block mb-2">Task:</label>
-          <input
-            type="text"
-            value={newTask}
-            onChange={handleNewTaskChange}
-            className="border border-gray-300 rounded px-2 py-1 mb-2"
-          />
+    <Draggable>
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="bg-white p-4 rounded-lg shadow-2xl cursor-move">
+          <h2 className="text-xl font-bold mb-4">Add Task</h2>
+          <div>
+            <label className="block mb-2">Task:</label>
+            <input
+              type="text"
+              value={newTask}
+              onChange={handleNewTaskChange}
+              className="border border-gray-300 rounded px-2 py-1 mb-2"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Priority:</label>
+            <input
+              type="text"
+              value={priority}
+              onChange={handlePriorityChange}
+              className="border border-gray-300 rounded px-2 py-1 mb-2"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Due Date:</label>
+            <input
+              type="text"
+              value={dueDate}
+              onChange={handleDueDateChange}
+              className="border border-gray-300 rounded px-2 py-1 mb-2"
+            />
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-500 text-white py-2 px-4 rounded"
+          >
+            Submit
+          </button>
+          <button onClick={handleClose}>close</button>
         </div>
-        <div>
-          <label className="block mb-2">Priority:</label>
-          <input
-            type="text"
-            value={priority}
-            onChange={handlePriorityChange}
-            className="border border-gray-300 rounded px-2 py-1 mb-2"
-          />
-        </div>
-        <div>
-          <label className="block mb-2">Due Date:</label>
-          <input
-            type="text"
-            value={dueDate}
-            onChange={handleDueDateChange}
-            className="border border-gray-300 rounded px-2 py-1 mb-2"
-          />
-        </div>
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-500 text-white py-2 px-4 rounded"
-        >
-          Submit
-        </button>
-        <button onClick={handleClose}>close</button>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
